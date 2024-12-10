231ee6e387a0f3e05027e7fe8a539867efa77986
Ogbuigbo Chijioke <chijiokejames82@gmail.com>
2024-12-10 19:36:20 +0100

    first commit

---
+++ app/utils/data.ts
@@ -0,0 +1,7 @@
+export const NAV_LINKS = [
+    { href: '/', key: 'home', label: 'Home' },
+    { href: '/about', key: 'about', label: 'About Us' },
+    { href: '/services', key: 'service', label: 'Services' },
+
+    { href: '/contact', key: 'contact', label: 'Contact' },
+  ];
---
+++ next.config.ts
@@ -0,0 +1,9 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    domains: ['aheioqhobo.cloudimg.io', 'images.unsplash.com', 'tinypic.host', 'res.cloudinary.com'],
+},
+};
+
+export default nextConfig;
