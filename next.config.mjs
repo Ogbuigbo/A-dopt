5ea60753c239ca905b2e870b48346d057c2c4e8d
Ogbuigbo Chijioke <chijiokejames82@gmail.com>
2024-12-12 12:53:50 +0100

    CHORE: update ui

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    // reactStrictMode: false,
+    images: {
+        domains: ['aheioqhobo.cloudimg.io', 'images.unsplash.com', 'tinypic.host', 'res.cloudinary.com'],
+    },
+};
+
+export default nextConfig;
--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  images: {
-    domains: ['aheioqhobo.cloudimg.io', 'images.unsplash.com', 'tinypic.host', 'res.cloudinary.com'],
-},
-};
-
-export default nextConfig;
